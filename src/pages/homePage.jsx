41739c6d9c71fb8d3f1c7078181b0d9f4bea341f
RupeshShandilya2003 <rupeshkshandilya@gmail.com>
2023-03-16 03:25:22 +0530

    imported heroSection.jsx

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import HeroSection from '../components/HeroSection/herosection'
 const HomePage = () => {
   return (
     <div>
-      
+      <HeroSection/>
     </div>
   )
 }

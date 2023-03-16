79e43adf723a3bed386f2e5d4e2d8df13d0c4050
RupeshShandilya2003 <rupeshkshandilya@gmail.com>
2023-03-16 12:32:17 +0530

    content added

--- src/components/HeroSection/heroSection.jsx
+++ src/components/HeroSection/heroSection.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import './style.css'
 
 const HeroSection = () => {
-  return (
-    <div>
-      
+  return <div className='hero-section-wrapper'>
+    <div className='flex absolute-center flex-col hero-section max-width'>
+      {/* -------------------------- Heading ------------------------- */}
+      <div className='hero-heading'>
+        Coming Soon &nbsp;
+        Polars Dev
+      </div>
+
+      {/* ----------------------- Sub-Heading ------------------------ */}
+      <div className='hero-subheading'>
+        Polaris for your Developement.
+      </div>
+      </div>      
     </div>
-  )
 }
 
 export default HeroSection;

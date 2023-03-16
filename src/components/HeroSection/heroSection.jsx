33e9aca2e324fa08bec47201d579c243bbe10d74
RupeshShandilya2003 <rupeshkshandilya@gmail.com>
2023-03-16 18:02:51 +0530

    added subscribe button

--- src/components/HeroSection/heroSection.jsx
+++ src/components/HeroSection/heroSection.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import Button from '../common/Button/button';
+import React, {useRef, useState} from 'react'
 import './style.css'
+import emailjs from '@emailjs/browser';
+import './subscribe.css'
+
 
 const HeroSection = () => {
+
+  const [user_email, setDepartment] = useState('');
+
+  const form = useRef();
+
+  const sendEmail = event => {
+
+    //To send Mail
+    emailjs.sendForm('service_kgc2jh8', 'template_pyl7ntg', form.current, 'fwVjw53fOy66fatzW')
+    .then((result) => {
+        console.log(result.text);
+        console.log("message sent");
+    }, (error) => {
+        console.log(error.text);
+    });
+
+    // To prevent page refresh
+    event.preventDefault();
+
+    //clear all input values after submit 
+    setDepartment('');
+};
+
+
   return <div className='hero-section-wrapper'>
     <div className='flex absolute-center flex-col hero-section max-width'>
       {/* -------------------------- Heading ------------------------- */}
@@ -14,9 +40,17 @@ const HeroSection = () => {
       <div className='hero-subheading'>
         Polaris for your Developement.
       </div>
-      <a href='#'>
-        <Button buttonText="Notify Me"/>
-      </a>
+
+<form ref={form} onSubmit={sendEmail}>
+      <div className='wrapper'> 
+      <input type="checkbox" id="click"/>
+      <label class="btn-1" for="click">Subscribe</label>
+      <div className='field'>
+        <input type="email" id="user_email" name='user_email' value={user_email}  placeholder="Enter Your Email" onChange={event => setDepartment(event.target.value)}/>
+        <input type="submit" for="click" class="btn-2" value="Subscribe" />
+      </div>
+      </div>
+      </form>
     </div> 
     </div>
 }
